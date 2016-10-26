cc589047c2714ef5e2e8a1eb6d0b8282ed2e515c
Toni Prada <toniprada@gmail.com>
2016-10-26 15:48:31 +0200

    Add listing endpoint, fix api_format mess

--- request_builder.py
+++ request_builder.py
@@ -5,31 +5,32 @@ class AirbnbRequestBuilder:
 
     BASE_URL = 'https://api.airbnb.com/v2'
 
-    def __init__(self, client_id, locale='en-US', currency='USD'):
+    def __init__(self, client_id, locale='en-US', currency='USD', api_format='v1_legacy_show'):
         self.client_id = client_id
         self.locale = locale
         self.currency = currency
+        self.api_format = api_format
 
-    def user(self, user_id, api_format='v1_legacy_show'):
-        params = {
-            '_format' : api_format
-        }
+    def user(self, user_id):
         endpoint = 'users/%s' % user_id
-        return self._build_request('GET', endpoint, params)
+        return self._build_request('GET', endpoint, {})
 
-    def user_listings(self, user_id, limit=50, offset=0, api_format='v1_legacy_long'):
+    def user_listings(self, user_id, limit=50, offset=0):
         params = {
             'user_id': user_id,
             '_limit': limit,
             '_offset': offset,
-            '_format' : api_format
         }
         return self._build_request('GET', 'listings', params)
+
+    def listing(self, listing_id):
+        endpoint = 'listings/%s' % listing_id
+        return self._build_request('GET', endpoint, {})
 
     def _build_request(self, method, endpoint, params):
         params['client_id'] = self.client_id
         params['locale'] = self.locale
         params['currency'] = self.currency
-        params['_format'] = 'v1_legacy_show'
+        params['_format'] = self.api_format
         url = '%s/%s?%s' % (self.BASE_URL, endpoint, urllib.urlencode(params))
         return {'method': method, 'url': url}
