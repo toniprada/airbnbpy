0a0af922401ed1987f710236a14d43ed3f052d2f
Toni Prada <toniprada@gmail.com>
2016-10-26 15:34:46 +0200

    Add user endpoint to request builder

---
+++ request_builder.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import urllib
+
+class AirbnbRequestBuilder:
+
+    BASE_URL = 'https://api.airbnb.com/v2'
+
+
+    def __init__(self, client_id, locale='en-US', currency='USD'):
+        self.client_id = client_id
+        self.locale = locale
+        self.currency = currency
+
+    def user(self, user_id):
+        endpoint = 'users/%s' % user_id
+        return self._build_request('GET', endpoint, {})
+
+    def _build_request(self, method, endpoint, params):
+        params['_format'] = 'v1_legacy_show'
+        params['client_id'] = self.client_id
+        params['locale'] = self.locale
+        params['EUR'] = self.currency
+        url = '%s/%s?%s' % (self.BASE_URL, endpoint, urllib.urlencode(params))
+        return {'method': method, 'url': url}
