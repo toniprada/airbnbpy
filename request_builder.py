63e63c0657bf33767946a8d8aab55115727bb44a
Toni Prada <toniprada@gmail.com>
2016-10-26 15:56:38 +0200

    Add paginated reviews endpoint, bring bad old _format

--- request_builder.py
+++ request_builder.py
@@ -5,32 +5,47 @@ class AirbnbRequestBuilder:
 
     BASE_URL = 'https://api.airbnb.com/v2'
 
-    def __init__(self, client_id, locale='en-US', currency='USD', api_format='v1_legacy_show'):
+    def __init__(self, client_id, locale='en-US', currency='USD'):
         self.client_id = client_id
         self.locale = locale
         self.currency = currency
-        self.api_format = api_format
 
-    def user(self, user_id):
+    def user(self, user_id, api_format='v1_legacy_show'):
+        params = {
+            '_format': api_format
+        }
         endpoint = 'users/%s' % user_id
-        return self._build_request('GET', endpoint, {})
+        return self._build_request('GET', endpoint, params)
 
-    def user_listings(self, user_id, limit=50, offset=0):
+    def user_reviews(self, user_id, role='all', limit=50, offset=0, api_format='for_mobile_client'):
+        params = {
+            'reviewee_id': user_id,
+            'role': role,
+            '_limit': limit,
+            '_offset': offset,
+            '_format': api_format
+        }
+        return self._build_request('GET', 'reviews', params)
+
+    def user_owned_listings(self, user_id, limit=50, offset=0, api_format='v1_legacy_long'):
         params = {
             'user_id': user_id,
             '_limit': limit,
             '_offset': offset,
+            '_format': api_format
         }
         return self._build_request('GET', 'listings', params)
 
-    def listing(self, listing_id):
+    def listing(self, listing_id, api_format='v1_legacy_for_p3'):
+        params = {
+            '_format': api_format
+        }
         endpoint = 'listings/%s' % listing_id
-        return self._build_request('GET', endpoint, {})
+        return self._build_request('GET', endpoint, params)
 
     def _build_request(self, method, endpoint, params):
         params['client_id'] = self.client_id
         params['locale'] = self.locale
         params['currency'] = self.currency
-        params['_format'] = self.api_format
         url = '%s/%s?%s' % (self.BASE_URL, endpoint, urllib.urlencode(params))
         return {'method': method, 'url': url}
